79468c5b1012c448ab7ca721ece27b311d5d9fc8
harri <mahonen.h@gmail.com>
2018-07-07 19:16:13 +0300

    added game start logic, will generate game locations now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 const HTTP_PORT = 18399
 const server = require('http').createServer()
 const io = require('socket.io')(server)
+const googleMaps = require('@google/maps').createClient({
+	key: 'AIzaSyAqcPrwNOpMl4IZL64QjfnBdPoE5dcZ28w',
+	Promise,
+})
+const collection = require('./cities')
+
+const makeGoogleMapsFetch = async () => {
+	const randomlySelectedCity = collection[Math.floor((Math.random() * collection.length) + 1)]
+	return googleMaps.geocode({ address: randomlySelectedCity })
+		.asPromise()
+		.then(response => response.json.results)
+		.then(data => data[0])
+		.then(data => ({
+			city: randomlySelectedCity,
+			location: data.geometry.location,
+		}))
+		.catch(err => {
+			console.log(err)
+		})
+}
+const generateRandomCities = async () => {
+	let results = []
+	Promise.all([
+		makeGoogleMapsFetch(),
+		makeGoogleMapsFetch(),
+		makeGoogleMapsFetch(),
+		makeGoogleMapsFetch(),
+		makeGoogleMapsFetch(),
+	])
+		.then(response => (
+			response.map(res => res)))
+		.then(values => {
+			results = values
+			console.log(results)
+		})
+}
 
 const generateUUID = () => {
 	let uuid = ''; let i; let
@@ -16,10 +52,11 @@ const generateUUID = () => {
 	return uuid
 }
 
-const newUser = name => ({
-	name, score: 0, id: generateUUID(), inGameId: false,
+const newUser = (user, data, gameid) => ({
+	name: data.name, score: 0, id: user.id, inGameId: gameid,
 })
 let usersInLobby = []
+const newLobby = () => ({ id: generateUUID() })
 const newGame = array => ({ array, gameId: generateUUID() })
 const onGoingGames = []
 const userList = []
@@ -27,9 +64,16 @@ const getUsersInGame = id => {
 	const users = userList.filter(obj => obj.gameId === id.gameId)
 	return users
 }
+let currentLobby = newLobby()
+const findUserObjectById = id => userList.find(obj => obj.id === id)
 
 io.on('connection', client => {
 	const startNewGame = () => {
+		// TODO initial City locations
+		const sendGameLocations = generateRandomCities()
+		io.in(currentLobby.id).emit('start', sendGameLocations)
+		console.log(userList)
+		currentLobby = newLobby()
 		onGoingGames.push(newGame(usersInLobby))
 		usersInLobby = []
 	}
@@ -39,20 +83,28 @@ io.on('connection', client => {
 	const updateUser = user => {
 		const users = getUsersInGame(user.gameId)
 		users.map(target => {
-			io.sockets.socket(target.id).emit(user.metadata)
+			io.socket(target.id).emit(user.metadata)
 		})
+	}
+	const handleConnect = data => {
+		const user = newUser(client, data, currentLobby.id)
+		userList.push(user)
+	}
+	const cleanUpAfterDC = () => {
+		const result = findUserObjectById(client.id)
+		const index = userList.indexOf(result)
+		userList.splice(index, 1)
 	}
 	const rewardUserForAnswer = user => {
 
 	}
 
-	const user = newUser(client.name)
-	usersInLobby.push(user)
-
+	client.join(currentLobby.id)
+	client.on('initial', handleConnect)
 	client.on('finish', finishGame)
 	client.on('start', startNewGame)
 	client.on('correct', rewardUserForAnswer)
 	client.on('update', updateUser)
-	client.on('disconnect', () => { client.destroy() })
+	client.on('disconnect', cleanUpAfterDC)
 })
 server.listen(HTTP_PORT)
